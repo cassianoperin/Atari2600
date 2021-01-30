b7d6c236e0a81043221bb26052b838e8216e145e
Cassiano <>
2021-01-30 00:36:22 -0300

    Implemented PF0

--- VGS/TIA_Background.go
+++ VGS/TIA_Background.go
@@ -2,45 +2,69 @@ package VGS
 
 import (
 	// "fmt"
-	"math/rand"
+	// "math/rand"
 	"image/color"
 	"github.com/faiface/pixel"
-
 )
 
 
 func drawBackground() {
 
-	// fmt.Printf("Beamer: %d\n", beamIndex)
-
-	R, G, B := NTSC(Memory[COLUBK])
-	imd.Color = color.RGBA{uint8(R), uint8(G), uint8(B), 255}
-
-
-
-	// // Draw
-	// imd.Push(pixel.V( (float64((beamIndex-1)  * 3) -68 ) * width	, (screenHeight * (1 - sizeYused)) + float64(232-line) * height ))
-	// imd.Push(pixel.V( (float64(beamIndex * 3) -68 ) * width 	, (screenHeight * (1 - sizeYused)) + float64(232-line) * height + height))
-	// imd.Rectangle(0)
-
-
 	// Draw the 3 sprites of CPU cycle
 	for i := 0 ; i < 3 ; i ++ {
-		// TEMPORARY - Random background colors
-		imd.Color = color.RGBA{uint8(rand.Intn(255)), uint8(rand.Intn(255)), uint8(rand.Intn(255)), 255}
 
-		// Draw
-		imd.Push(pixel.V( (float64((beamIndex-1) * 3) - 68 + float64(i) ) * width	, (screenHeight * (1 - sizeYused)) + float64(232-line) * height ))
-		imd.Push(pixel.V( (float64((beamIndex-1) * 3) - 68 + float64(i) + 1) * width 	, (screenHeight * (1 - sizeYused)) + float64(232-line) * height + height))
-		imd.Rectangle(0)
+		// Define the pixel position
+		pixel_position := ( (int(beamIndex)-1) * 3 ) - 68 + i + 1
 
-		// Count draw operations number per second
-		counter_DPS ++
+		// Dont draw first two sprites outside screen (-2 and -1 X position)
+		if pixel_position > 0 {
+
+			// Set the background color as default
+			R, G, B := NTSC(Memory[COLUBK])
+			imd.Color = color.RGBA{uint8(R), uint8(G), uint8(B), 255}
+
+
+
+			// PF 0
+			if pixel_position <= 16 {
+
+				// fmt.Printf("%d\tPF0: %b\t%b\tPF_BIT: %d\n", pixel_position, Memory[PF0], Memory[PF0] >> byte(pf_bit) & 0x01, pf_bit)
+
+				// If the bit is 1, set the color of the playfield
+				if Memory[PF0] >> byte(pf_bit) & 0x01 == 1 {
+					R, G, B := NTSC(Memory[COLUPF])
+					imd.Color = color.RGBA{uint8(R), uint8(G), uint8(B), 255}
+				}
+
+				// Each 4 sprites increase the index (playfield bit)
+				if pixel_position % 4 == 0 {
+					// fmt.Println("ENTROU")
+					pf_bit ++
+				}
+
+				// Reset pixel_position for PF0 for the next line
+				if pixel_position == 16 {
+					pf_bit = 4
+				}
+			}
+
+
+
+
+
+
+			// TEMPORARY - Random background colors
+			// imd.Color = color.RGBA{uint8(rand.Intn(255)), uint8(rand.Intn(255)), uint8(rand.Intn(255)), 255}
+
+			// Draw
+			imd.Push(pixel.V( float64(pixel_position - 1)		* width	, (screenHeight * (1 - sizeYused)) + float64(232-line) * height ))
+			imd.Push(pixel.V( float64(pixel_position)				* width , (screenHeight * (1 - sizeYused)) + float64(232-line) * height + height))
+			imd.Rectangle(0)
+
+			// Count draw operations number per second
+			counter_DPS ++
+		}
+
 	}
-
-
-
-
-
 
 }
--- VGS/Variables.go
+++ VGS/Variables.go
@@ -109,6 +109,8 @@ var (
 	// PF0(4,5,6,7) | PF1 (7,6,5,4,3,2,1,0) | PF2 (0,1,2,3,4,5,6,7)
 	playfield			[40]byte			//Improve to binary
 	pixelSize			float64 = 4.0		// 80 lines (half screen) / 20 PF0, PF1 and PF2 bits
+	pf_bit				byte = 4				// PF0 bit index
+
 
 	// ------------------------------ Graphics ------------------------------ //
 	// Screen Size
